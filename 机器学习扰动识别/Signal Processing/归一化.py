409c401d44ac8ed5f468031dcb908e4df9936cc8
xinqi <791336077@qq.com>
2020-08-16 22:47:41 +0800

    L2范数归一化

--- 机器学习扰动识别/Signal Processing/归一化.py
+++ 机器学习扰动识别/Signal Processing/归一化.py
@@ -1,10 +1,14 @@
 import scipy.io as scio
 datafile = 'datajiao.mat'
 datajiao = scio.loadmat(datafile)
+datajiao=datajiao['datajiao']
 print(datajiao)
 
-import numpy as np  
-arr = np.asarray(datajiao)
-for x in arr:
-  x = float(x - arr.mean())/arr.std()
-  print(x)
+import numpy as np
+jiaoguiyihua=[]
+for i in datajiao:
+    i_norm=np.linalg.norm(i, ord=2)
+    for x in i:
+        fan=x/i_norm
+        jiaoguiyihua.append(fan)
+print(jiaoguiyihua)
