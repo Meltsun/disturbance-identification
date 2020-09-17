51bd0f6090a345d4efd0646a76bfd99d3dd4863f
xinqi <791336077@qq.com>
2020-09-17 21:18:08 +0800

    shiyu

--- 机器学习扰动识别/Signal Processing/Time.py
+++ 机器学习扰动识别/Signal Processing/Time.py
@@ -35,21 +35,21 @@ def time_draw(data:np.ndarray):
 
 	#波形特征
 	#峭度（波形的平缓程度）
-	feature[9]=np.sqrt(np.mean(pow(data,4))/feature[5]
+	feature[9]=np.sqrt(np.mean(pow(data,4))/feature[5])
 
 	#偏度（数据分布非对称程度）
 	feature[10]=np.mean(pow(((data-feature[3])/np.sqrt(feature[7])),3))
 
 	#裕度因子（对冲击敏感）
-	feature[11]=feature[2]/pow(np.mean(np.sqrt(abs(data))),2)
+	feature[B]=feature[2]/pow(np.mean(np.sqrt(abs(data))),2)
 
 	#波形因子（对峰值敏感）
-	feature[12]=feature[5]/feature[4]
+	feature[C]=feature[5]/feature[4]
 
 	#峰值因子
-	feature[13]=feature[2]/feature[5]
+	feature[D]=feature[2]/feature[5]
 
 	#脉冲因子
-	feature[14]=feature[2]/feature[4]
+	feature[E]=feature[2]/feature[4]
 
 	return feature
