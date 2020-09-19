032e2a72be77479a2f00187c3d1a7340f183a0e4
Meltsun <1327616444@qq.com>
2020-09-19 17:40:08 +0800

    20200919

--- 机器学习扰动识别/Signal Processing/Origin_data.py
+++ 机器学习扰动识别/Signal Processing/Origin_data.py
@@ -1,5 +1,3 @@
-
-
 points=[35,259,859]
 norms=[131626.41826016537, 348089.33621126635, 171083.7945803167, 532644.7420636009, 240586.45893732258] 
 nTime=1111#每个点的时域数据数
@@ -7,7 +5,7 @@ nTime=1111#每个点的时域数据数
 import numpy as np
 #打开文件，导入数据
 file=[open("data_Nothing.csv").readlines(),open("data_Pass.csv").readlines(),open("data_Water.csv").readlines(),open("data_Knock.csv").readlines(),open("data_Climb.csv").readlines()]
-originSet=np.empty([15,nTime])
+originSet=np.empty([len(file)*len(points),nTime])
 print("csv文件读取成功")
 
 ##得到原始数据集originaSet（2维列表） 模式集targetSet（1维列表
